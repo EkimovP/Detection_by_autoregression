01028de9af100eea6d974cc339c5d8193434bf5d
EkimovP <1paha1212@mail.ru>
2023-10-27 21:51:04 +0300

    init

---
+++ resource.h
@@ -0,0 +1,34 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется IT2.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_IT2_DIALOG                  102
+#define IDR_MAINFRAME                   128
+#define IDC_SINS                        1001
+#define IDC_f1                          1002
+#define IDC_f2                          1003
+#define IDC_f3                          1004
+#define IDC_fd                          1005
+#define IDC_N                           1006
+#define IDC_nn1                         1007
+#define IDC_n1                          1008
+#define IDC_n2                          1009
+#define IDC_MIS                         1010
+#define IDC_nn2                         1011
+#define IDC_t                           1012
+#define IDC_nn4                         1013
+#define IDC_TT                          1013
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
